bd742c09fe9e62e51dd28c6694a905584893a10d
marcelrm11 <marcelrm11@gmail.com>
2022-11-21 09:14:56 +0000

    changed to numbers in test

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const {sum, oneEuroIs} = require("./app");
+const {sum} = require("./app");
 
 test("adds 14 + 9 to equal 23", () => {
     let total = sum(14, 9);
@@ -8,20 +8,20 @@ test("adds 14 + 9 to equal 23", () => {
 test("One euro should be 1.206 dollars", () => {
     const {fromEuroToDollar} = require("./app");
     const dollars = fromEuroToDollar(3.5);
-    const expected = 3.5 * oneEuroIs.USD;
+    const expected = 3.5 * 1.206;
     expect(dollars).toBeCloseTo(expected);
 });
 
 test("One dollar should be 106.05 yens", () => {
     const {fromDollarToYen} = require("./app");
     const yens = fromDollarToYen(3.5);
-    const expected = 3.5 / oneEuroIs.USD * oneEuroIs.JPY;
+    const expected = 3.5 / 1.206 * 127.9;
     expect(yens).toBeCloseTo(expected);
 });
 
 test("1000 yens should be 6.25 pounds", () => {
     const {fromYenToPound} = require("./app");
     const pounds = fromYenToPound(1000);
-    const expected = 1000 / oneEuroIs.JPY * oneEuroIs.GBP;
+    const expected = 1000 / 127.9 * 0.8;
     expect(pounds).toBeCloseTo(expected);
 });
